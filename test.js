2ab7e24a9ef0277fdc06d0da70909a658785faf7
robertjd <robertjd@users.noreply.github.com>
2014-06-11 15:25:55 -0700

    what crypto support does the travis env have?

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 console.log('faux test!');
 console.log('printing env, don\'t try this at home!');
 console.log(process.env);
+var crypto = require('crypto');
+var ciphers = crypto.getCiphers();
+console.log(ciphers);
+var hashes = crypto.getHashes();
+console.log(hashes); 
 process.exit(0);
